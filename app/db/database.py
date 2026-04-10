06873270223357adbd1d37f57b016eea1318ddbd
YSB Saji <ysbsaji999@gmail.com>
2026-04-10 10:27:13 +0530

    connect render postgres

--- app/db/database.py
+++ app/db/database.py
@@ -1,10 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://linkhub_user:postgres@localhost:5432/linkhub"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
